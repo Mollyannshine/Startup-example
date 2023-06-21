13730aa97c2dff93febaedcc9b0361857eb4f2a7
Mollyannshine <mollyann252@yahoo.com>
2023-06-20 20:15:39 -0600

    added router

--- src/app.jsx
+++ src/app.jsx
@@ -2,30 +2,49 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Map } from './map/map';
+import { Form } from './form/form';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
 export default function App() {
   return (
+    <BrowserRouter>
     <div className='body bg-dark text-light'>
       <header className='container-fluid'>
-        <nav className='navbar fixed-top navbar-dark'>
-          <div className='navbar-brand'>
-            What The Dog Doing<sup>&reg;</sup>
-          </div>
-          <menu className='navbar-nav'>
-            <li className='nav-item'>
-              <a className='nav-link' href='index.html'>
-                Map
-              </a>
-            </li>
-            <li className='nav-item'>
-              <a className='nav-link' href='play.html'>
-                Form
-              </a>
-            </li>
-          </menu>
-        </nav>
+      <nav className='navbar fixed-top navbar-dark'>
+        <div className='navbar-brand'>
+          What The Dog Doing<sup>&reg;</sup>
+        </div>
+        <menu className='navbar-nav'>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='login'>
+              Login
+            </NavLink>
+          </li>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='map'>
+              Map
+            </NavLink>
+          </li>
+          <li className='nav-item'>
+            <NavLink className='nav-link' to='form'>
+              Add Sighting
+            </NavLink>
+          </li>
+        </menu>
+      </nav>
       </header>
 
-      <main>App components go here</main>
+      <Routes>
+        <Route path='/' element={<Login />} exact />
+        <Route path='/login' element={<Login />} />
+        <Route path='/map' element={<Map />} />
+        <Route path='/form' element={<Form />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
 
       <footer className='bg-dark text-white-50'>
         <div className='container-fluid'>
@@ -36,5 +55,10 @@ export default function App() {
         </div>
       </footer>
     </div>
+  </BrowserRouter>
   );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Huh. Couldn't find that page.5</main>;
 }
